db37cd140bc0f026c9486e6b717b339a4f7b5196
Scott Mangiapane <scottmangiapane@gmail.com>
2024-01-01 20:57:39 -0700

    Dockerized UI

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
+import react from '@vitejs/plugin-react-swc';
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  }
+});
