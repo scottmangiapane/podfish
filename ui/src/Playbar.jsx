1ca1cf87b3b4180e96a4c6e67447ff26afedb3b0
Scott Mangiapane <scottmangiapane@gmail.com>
2024-02-05 00:50:22 -0600

    Allowed playbar metadata to use all available space; made play/pause dynamic

--- ui/src/Playbar.jsx
+++ ui/src/Playbar.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 import { RootContext } from "./Root";
 
@@ -6,24 +6,28 @@ import "./Playbar.css";
 
 function Playbar() {
   const { state } = useContext(RootContext);
+  const [isPaused, setIsPaused] = useState(false);
 
   const { episodeTitle, podcastTitle } = state.nowPlaying;
 
+  const playPause = () => {
+    setIsPaused(!isPaused);
+  };
+
   return (
     <div className="app-content playbar">
-      <div>
+      <div className="playbar-stretch">
         <p className="truncate">{ episodeTitle }</p>
         <p className="text-light truncate">{ podcastTitle }</p>
       </div>
-      <div>
-        <div className="align-center playbar-symbol-group">
-          <span className="symbol symbol-btn">replay_10</span>
-          <span className="symbol symbol-btn symbol-play-pause">play_circle</span>
-          {/* <span className="symbol symbol-btn symbol-play-pause">pause_circle</span> */}
-          <span className="symbol symbol-btn">forward_30</span>
-        </div>
+      <div className="playbar-symbol-group">
+        <span className="symbol symbol-btn">replay_10</span>
+        <span className="symbol symbol-btn symbol-play-pause" onClick={ playPause }>
+          { (isPaused) ? "play_circle" : "pause_circle" }
+        </span>
+        <span className="symbol symbol-btn">forward_30</span>
       </div>
-      <div>
+      <div className="playbar-stretch">
         <div className="align-right">
           <span className="symbol symbol-btn">volume_up</span>
           {/* <span className="symbol symbol-btn">volume_down</span> */}
